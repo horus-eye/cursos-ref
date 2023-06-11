d1c58023f07771656cedb4d4e702b914b196b0de
jhon <juanignacioperezmartinez59@gmail.com>
2023-06-11 11:57:43 -0600

    modificando imagenes para desaparecer

--- js.js
+++ js.js
@@ -2,6 +2,13 @@ $(() => {
     $(".content").hide();
     $(".sales").hide();
     $("a").hide();
+
     $(".c1").click(e => { e.preventDefault(); $(".cont1").slideToggle("fast"); $("a").slideToggle("fast") });
     $(".c2").click(e => { e.preventDefault(); $(".cont2").slideToggle("fast"); $(".sales").slideToggle("fast") });
+
+    $(".sales").click(function (e) {
+        e.preventDefault();
+        $(this).slideToggle("smooth");
+    });
+
 });
